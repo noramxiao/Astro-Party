3f3c0458b9239d8bfa74dbca511c6ad1d5e24718
myang2@caltech.edu <myang2@caltech.edu>
2024-05-29 23:15:04 +0000

    refactor entity_info related

--- library/entities.c
+++ library/entities.c
@@ -35,14 +35,9 @@ entity_info_free(entity_info_t *info) {
 	free(info);
 }
 
-entity_info_t *make_type_info(entity_type_t type) {
-  entity_info_t *info = malloc(sizeof(entity_info_t));
-  info->type = type;
-  return info;
-}
-
 entity_type_t get_type(body_t *body) {
-  return *(entity_type_t)(body_get_info(body)->type);
+	entity_info_t *info = body_get_info(body);
+  return info->type;
 }
 
 body_t *make_ship(vector_t centroid, rgb_color_t color, vector_t init_velocity) {
