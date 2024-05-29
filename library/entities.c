ad35b54d01235cb714abec1fe593c12e1622a9c8
jluo2@caltech.edu <jluo2@caltech.edu>
2024-05-29 23:04:05 +0000

    asdf

--- library/entities.c
+++ library/entities.c
@@ -26,6 +26,16 @@ typedef struct entity_info {
 	entity_type_t type;
 } entity_info_t;
 
+entity_info_t *make_type_info(entity_type_t type) {
+  entity_info *info = malloc(sizeof(entity_info_t));
+  info->type = type;
+  return info;
+}
+
+entity_type_t get_type(body_t *body) {
+  return *(entity_type_t)(body_get_info(body)->type);
+}
+
 body_t *make_ship(vector_t centroid, rgb_color_t color, vector_t init_velocity) {
 	list_t *ship = make_triangle(centroid, SHIP_BASE, SHIP_HEIGHT, color, init_velocity);
 	body_t *ret = body_init_with_info(ship, SHIP_MASS, )
