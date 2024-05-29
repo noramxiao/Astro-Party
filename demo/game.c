fb9d2cf0363c106d877735b61ad2c38c2260dbc7
nxiao@caltech.edu <nxiao@caltech.edu>
2024-05-29 22:43:21 +0000

    aslkfj

--- demo/game.c
+++ demo/game.c
@@ -13,24 +13,29 @@
 
 const vector_t MIN = {0, 0};
 const vector_t MAX = {1000, 500};
+const size_t INITIAL_CAPACITY = 5;
+const size_t WIN_SCORE = 5;
+const size_t N_PLAYERS = 2;
 
 const double SHIP_MASS = 10.0;
 
-enum Mode {
+enum mode {
   HOME,
   GAME,
   POST_GAME
 };
 
 struct state {
-  enum Mode mode; // Keeps track of what page game is on
+  enum mode mode; // Keeps track of what page game is on
   size_t P1_score;
   size_t P2_score;
   
+  list_t *assets;
   scene_t *scene;
 };
 
 void on_key(char key, key_event_type_t type, double held_time, state_t *state) {
+  /*TODO: edit with Space Wars implementation*/
   body_t *froggy = scene_get_body(state->scene, 0);
   vector_t translation = (vector_t){0, 0};
   if (type == KEY_PRESSED && type != KEY_RELEASED) {
@@ -63,69 +68,61 @@ state_t *emscripten_init() {
   asset_cache_init();
   sdl_init(MIN, MAX);
   state_t *state = malloc(sizeof(state_t));
-  state->points = 0;
-  srand(time(NULL));
+  state->mode = HOME;
+  state->P1_score = 0;
+  state->P2_score = 0;
   state->scene = scene_init();
-  state->body_assets = list_init(2, (free_func_t)asset_destroy);
+  srand(time(NULL));
 
-  body_t *froggy = make_frog(OUTER_RADIUS, INNER_RADIUS, VEC_ZERO);
-  body_set_centroid(froggy, RESET_POS);
-
-  scene_add_body(state->scene, froggy);
-
+  // BACKGROUND
   SDL_Rect background_bbox = (SDL_Rect){
       .x = MIN.x, .y = MIN.y, .w = MAX.x - MIN.x, .h = MAX.y - MIN.y};
   asset_t *background_asset =
       asset_make_image(BACKGROUND_PATH, background_bbox);
   list_add(state->body_assets, background_asset);
 
-  asset_t *frog_asset = asset_make_image_with_body(FROGGER_PATH, froggy);
-  list_add(state->body_assets, frog_asset);
-
-  for (size_t r = 3; r < ROWS + 3; r++) {
-    double cx = 0;
-    double cy = r * V_STEP + body_get_centroid(froggy).y;
-    double multiplier = 0;
-    if (r % 2 == 0) {
-      multiplier = 1;
-    } else {
-      multiplier = -1;
-    }
-    if ((double)rand() / RAND_MAX < VEL_MULT_PROB) {
-      multiplier *= EXTRA_VEL_MULT;
-    }
-    while (cx < MAX.x) {
-      double w = rand_double(OBS_WIDTHS.x, OBS_WIDTHS.y);
-      body_t *obstacle = make_obstacle(w, OBSTACLE_HEIGHT, (vector_t){cx, cy});
-      cx += w + rand_double(OBS_SPACING.x, OBS_SPACING.y);
-
-      body_set_velocity(obstacle, vec_multiply(multiplier, BASE_OBJ_VEL));
-      scene_add_body(state->scene, obstacle);
-
-      create_collision(state->scene, froggy, obstacle, reset_user_handler, NULL,
-                       0);
-
-      asset_t *log_asset = asset_make_image_with_body(LOG_PATH, obstacle);
-      list_add(state->body_assets, log_asset);
-    }
-  }
   sdl_on_key((key_handler_t)on_key);
   return state;
 }
 
+void update_score(state_t *state) {
+  bool p1 = false;
+  bool p2 = false;
+  size_t n_bodies = scene_bodies(state->scene);
+
+  for (size_t i = 0; i < n_bodies; i++) {
+    void *info = body_get_info(scene_get_body(state->scene, i));
+    if (info == P1_SHIP || info == P1_PILOT) {
+      p1 = true;
+    } else if (info == P2_SHIP || info == P2_PILOT) {
+      p2 = true;
+    }
+  }
+
+  if (!(p1 && p2)) {
+    if (p1) {
+      state->P1_score++;
+    } else {
+      state->P2_score++;
+    }
+  }
+}
+
+void render_scene(state_t *state, void *aux) {
+  switch (state->mode) {
+    case HOME:
+
+  }  
+}
+
 bool emscripten_main(state_t *state) {
   double dt = time_since_last_tick();
-  player_wrap_edges(state);
-  for (int i = 1; i < scene_bodies(state->scene); i++) {
-    wrap_edges(scene_get_body(state->scene, i));
-  }
-  sdl_clear();
-  for (size_t i = 0; i < list_size(state->body_assets); i++) {
-    asset_render(list_get(state->body_assets, i));
-  }
-  sdl_show();
-
   scene_tick(state->scene, dt);
+
+  update_score(state);
+  if (state->P1_score > WIN_SCORE || state->P2_score > WIN_SCORE) { return true; }
+
+  render_scene(state->scene, NULL);
   return false;
 }
 
